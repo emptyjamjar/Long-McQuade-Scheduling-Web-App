569e63b59c7ce11ded6c1b65593156794d2957f5
moham <iga907@usask.ca>
2023-10-14 06:22:14 +0300

    implementation of SecurityContextHolder.getContext().getAuthentication()

--- src/main/java/com/Long/McQuade/SchedulingSystem/controllers/homepageController.java
+++ src/main/java/com/Long/McQuade/SchedulingSystem/controllers/homepageController.java
@@ -1,5 +1,7 @@
 package com.Long.McQuade.SchedulingSystem.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ public class homepageController {
     @GetMapping("/")
     public String showHomepage() {
 
-        return "This is the homepage";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return "This is the homepage and user " + authentication.getName() + " is logged in";
     }
 }
