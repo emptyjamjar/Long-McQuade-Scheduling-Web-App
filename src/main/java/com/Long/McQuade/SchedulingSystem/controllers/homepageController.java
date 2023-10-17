42f4a6de9cd2e5b03152e6c96277d42124a671d7
moham <iga907@usask.ca>
2023-10-18 01:56:22 +0300

    case statements for each entity

--- src/main/java/com/Long/McQuade/SchedulingSystem/controllers/homepageController.java
+++ src/main/java/com/Long/McQuade/SchedulingSystem/controllers/homepageController.java
@@ -1,5 +1,9 @@
 package com.Long.McQuade.SchedulingSystem.controllers;
 
+import com.Long.McQuade.SchedulingSystem.service.StudentServiceImpl;
+import com.Long.McQuade.SchedulingSystem.service.TeacherServiceImpl;
+import com.Long.McQuade.SchedulingSystem.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +14,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/homepage")
 public class homepageController {
 
+    @Autowired
+    private StudentServiceImpl studentService;
+
+    @Autowired
+    private TeacherServiceImpl teacherServiceImpl;
+
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping("/")
     public String showHomepage() {
 
+        String message = "";
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        return "This is the homepage and user " + authentication.getName() + " is logged in";
+        String authority = authentication.getAuthorities().iterator().next().getAuthority();
+
+        String identificationNumber = authentication.getName();
+
+        switch (authority) {
+            case "ADMIN":
+
+                message += "Your first name is: " + userService.findByUserNumber(identificationNumber).getFirstName() + " \n\n\n";
+                break;
+
+            case "TEACHER":
+                message += "Your first name is: " + teacherServiceImpl.findTeacherByTeacherNumber(identificationNumber).getFirstName() + " \n";
+                break;
+
+            case "STUDENT":
+                message += "Your first name is: " + studentService.findStudentByStudentNumber(identificationNumber).getFirstName() + " \n";
+                break;
+        }
+
+        message += "\nThis is the homepage and user " + authentication.getName() + " is logged in";
+
+
+
+        return message;
     }
 }
