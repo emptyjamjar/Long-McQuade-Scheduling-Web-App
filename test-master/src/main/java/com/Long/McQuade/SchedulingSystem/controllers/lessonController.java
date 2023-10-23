e1cc2ef84913a20865443a64575b2f9459857373
jasminepawliw <jasminepawliw99@gmail.com>
2023-10-23 12:21:04 -0600

    Initial commit * Downloaded master branch from gitLab * Currently working on homepage * Going to attempt connecting front end and back end

---
+++ test-master/src/main/java/com/Long/McQuade/SchedulingSystem/controllers/lessonController.java
@@ -0,0 +1,60 @@
+package com.Long.McQuade.SchedulingSystem.controllers;
+
+
+import com.Long.McQuade.SchedulingSystem.entities.Lesson;
+import com.Long.McQuade.SchedulingSystem.service.LessonServiceImpl;
+import com.Long.McQuade.SchedulingSystem.service.StudentServiceImpl;
+import com.Long.McQuade.SchedulingSystem.service.TeacherServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lessons")
+public class lessonController {
+
+    @Autowired
+    private LessonServiceImpl lessonService;
+
+    @Autowired
+    private StudentServiceImpl studentService;
+
+    @Autowired
+    private TeacherServiceImpl teacherService;
+
+    @GetMapping("/")
+    public List<Lesson> showAllLessons() {
+        return lessonService.findAll();
+    }
+
+    @GetMapping("/{lessonNumber}")
+    public Lesson showLessonByLessonNumber(@PathVariable("lessonNumber") String lessonNumber) {
+
+        int id = Character.getNumericValue(lessonNumber.charAt(1));
+        return lessonService.findBy(id);
+    }
+
+    @PostMapping("/newlesson")
+    public Lesson createlesson(@RequestBody Lesson lesson) {
+
+        lessonService.save(lesson);
+        lesson.setLessonNumber("L" + lesson.getId());
+        return lessonService.save(lesson);
+    }
+
+    @PutMapping("/updatelesson")
+    public Lesson updateLesson(@RequestBody Lesson lesson) {
+
+        Lesson newLesson = lessonService.save(lesson);
+        return newLesson;
+    }
+
+    @DeleteMapping("/deletelesson/{lessonNumber}")
+    public String deleteLesson(@PathVariable("lessonNumber") String lessonNumber) {
+
+        int id = Character.getNumericValue(lessonNumber.charAt(1));
+        return lessonService.deleteByID(id);
+    }
+
+}
