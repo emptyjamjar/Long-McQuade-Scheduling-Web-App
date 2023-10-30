ea8860ddfad4958796dd288dfa58b39e7cde9fbf
Enirehtakcodes <126849523+Enirehtakcodes@users.noreply.github.com>
2023-10-29 18:47:39 -0600

    Prevented reload on sidebar and added a Manage Users page

--- longMcQuadeScheduling/src/components/Navbar.tsx
+++ longMcQuadeScheduling/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import {Link} from "react-router-dom"
 
 /* 
 Navbar is the left aligned orange column with links to different pages of the website.
@@ -16,8 +17,8 @@ function PageNavbar() {
         id="navbar"
         sticky="top"
         style={{
-          width: "200px",
-          height: "86.9vh",
+          width: "180px",
+          height: "100vh",
           fontFamily: "Raleway",
           textTransform: "uppercase",
         }}
@@ -29,33 +30,34 @@ function PageNavbar() {
             justify
             variant="underline"
             className="flex-column"
-            activeKey={location.pathname}
             style={{
               fontSize: "14px",
               fontWeight: "lighter",
+              display: "flex",
               alignItems: "center",
+              paddingTop: "40%",
             }}
           >
-            <Nav.Link style={linkStyle} href="/">
-              Home
-            </Nav.Link>
-            <Nav.Link style={linkStyle} href="/account">
+            <Nav.Link as={Link} style={linkStyle} to="/account">
               Account
             </Nav.Link>
-            <Nav.Link style={linkStyle} href="/messages">
+            <Nav.Link as={Link} style={linkStyle} to="/messages">
               Message
             </Nav.Link>
-            <Nav.Link style={linkStyle} href="/calendar">
+            <Nav.Link as={Link} style={linkStyle} to="/calendar">
               Calendar
             </Nav.Link>
-            <Nav.Link style={linkStyle} href="/my_schedule">
+            <Nav.Link as={Link} style={linkStyle} to="/my_schedule">
               My Schedule
             </Nav.Link>
-            <Nav.Link style={linkStyle} href="/profiles">
+            <Nav.Link as={Link} style={linkStyle} to="/profiles">
               Teacher Profiles
             </Nav.Link>
-            <Nav.Link style={linkStyle} href="/contact">
+            <Nav.Link as={Link} style={linkStyle} to="/contact">
               Contact
+            </Nav.Link>
+            <Nav.Link as={Link} style={linkStyle} to="/manageUsers">
+              Manage Users
             </Nav.Link>
           </Nav>
         </Container>
