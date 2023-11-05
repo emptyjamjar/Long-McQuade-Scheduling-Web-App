04e93b0542e609378f6c1d8701810ebf171b1df7
Enirehtakcodes <126849523+Enirehtakcodes@users.noreply.github.com>
2023-11-04 19:13:50 -0600

    added a registration page that has a form  to the manage users page

--- longMcQuadeScheduling/src/App.tsx
+++ longMcQuadeScheduling/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
 import Topbar from "./components/Topbar/Topbar";
 import Home from "./pages/index/index";
 import Account from "./pages/account/account";
 import Calendar from "./pages/calendar/calendar";
 import Contact from "./pages/contact";
 import ManageUsers from "./pages/manageUsers";
+import AddStudent from "./pages/addstudent";
 
 /* 
   The main application for all components 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/manageUsers" element={<ManageUsers />} />
+        <Route path="/addstudent" element={<AddStudent />} />
       </Routes>
     </Router>
   );
