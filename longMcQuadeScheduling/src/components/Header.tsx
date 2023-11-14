9eee9b96ad2194e5b4c115f7a2705918eb5327a1
jasminepawliw <jasminepawliw99@gmail.com>
2023-11-13 19:32:16 -0600

    fixing merge conflicts

--- longMcQuadeScheduling/src/components/Header.tsx
+++ longMcQuadeScheduling/src/components/Header.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 
+interface H1Props {
+  message: string;
+  name: string;
+}
+
 /* 
     The welcome text for the homepage. Uses styled-components for styling (no css file).
     TODO: need to update with user's name when connected to code (i.e., getName()). {name} will become 
     {getName()}.
  */
 const H1 = styled.h1`
-  position: relative;
   font-family: "Roboto";
   color: #080f0f;
   font-weight: 500;
-  font-size: 55px;
+  font-size: 50px;
   width: fit-content;
   margin: 0 0 3rem 0;
   height: fit-content;
@@ -19,13 +23,17 @@ const H1 = styled.h1`
   border-image: linear-gradient(to right, #f5f1ed, #f68b1f) 1;
 `;
 
-const WelcomeText = () => {
-  const name = "Ruth";
+const WelcomeText = ({ message, name }: H1Props) => {
+  // const name = "Ruth";
 
   if (name) {
-    return <H1 className="display-1">Welcome {name}</H1>;
+    return (
+      <H1 className="display-1">
+        {message} {name}
+      </H1>
+    );
   }
-  return <H1 className="display-1">Welcome</H1>;
+  return <H1 className="display-1">{message}</H1>;
 };
 
 export default WelcomeText;
