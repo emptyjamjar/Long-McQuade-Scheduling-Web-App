df3fbf7b13d9bcaa7dc17cd66deff46b7c5ff5f1
Enirehtakcodes <126849523+Enirehtakcodes@users.noreply.github.com>
2023-11-15 22:16:55 -0600

    Worked more on teachers Profile

--- longMcQuadeScheduling/src/pages/teachersProfiles.tsx
+++ longMcQuadeScheduling/src/pages/teachersProfiles.tsx
@@ -1,12 +1,24 @@
 import Header from "../components/Header";
+import React from 'react';
 
-const teachersProfiles = () => {
+interface TeacherProfileProp{
+  names: string;
+  intro: string;
+  image: string;
+}
+
+const TeachersProfiles: React.FC<TeacherProfileProp>= ({ names, intro, image })=>{
   return <div style={{ width: "100vw" }}>
   <div id="teachersProfiles">
     <Header message="Teacher Profiles" name="" />
+    
+      <h2>{names}</h2>
+      <p>{intro}</p>
+      <img src={image} alt={`${names}'s profile`} />
+    
   </div>
   
 </div>
 };
 
-export default teachersProfiles;
+export default TeachersProfiles;
